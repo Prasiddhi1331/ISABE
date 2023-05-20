857fbf9c101812c4f0ea0c9a77ba848de713e3d6
Piyush <piyush@pop-os.localdomain>
2023-05-20 10:51:14 +0530

    fixes

--- JS/Nav.js
+++ JS/Nav.js
@@ -12,11 +12,11 @@
       $('.dropdown-content').hide();
     });
     // Toggle open and close nav styles on click
-    $('#nav-toggle').click(function () {
+    $('.toggle-btn').click(function () {
       $('nav ul').slideToggle();
     });
     // Hamburger to X toggle
-    $('#nav-toggle').on('click', function () {
+    $('.toggle-btn').on('click', function () {
       this.classList.toggle('active');
     });
   }); // end DOM ready
