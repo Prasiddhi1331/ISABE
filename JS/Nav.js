c291701302d29de3cd24ff2cf13439fa11b183b7
Piyush <piyush@pop-os.localdomain>
2023-05-20 10:24:22 +0530

    Piddi nav fix

--- JS/Nav.js
+++ JS/Nav.js
@@ -2,14 +2,14 @@
   $(function () { // DOM ready
     // If a link has a dropdown, add sub menu toggle.
     $('nav ul li a:not(:only-child)').click(function (e) {
-      $(this).siblings('.nav-dropdown').toggle();
+      $(this).siblings('.dropdown-content').toggle();
       // Close one dropdown when selecting another
-      $('.nav-dropdown').not($(this).siblings()).hide();
+      $('.dropdown-content').not($(this).siblings()).hide();
       e.stopPropagation();
     });
     // Clicking away from dropdown will remove the dropdown class
     $('html').click(function () {
-      $('.nav-dropdown').hide();
+      $('.dropdown-content').hide();
     });
     // Toggle open and close nav styles on click
     $('#nav-toggle').click(function () {
